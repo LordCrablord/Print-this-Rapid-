ce242c8c90e005a9b64aa396b584360c1fb4fb66
LordCrablord <56042616+LordCrablord@users.noreply.github.com>
2023-12-07 21:42:06 +0200

    Help button present

--- Assets/Scripts/GameManager.cs
+++ Assets/Scripts/GameManager.cs
@@ -8,13 +8,23 @@ public class GameManager : Singleton<GameManager>
     public int happyCustomer = 0;
     [SerializeField] GameObject gameUI;
     [SerializeField] GameObject optionUI;
+    [SerializeField] GameObject helpUI;
 
     private void Update()
     {
         if (Input.GetKeyDown(KeyCode.Escape))
         {
             optionUI.SetActive(!optionUI.activeInHierarchy);
+        }else if (Input.GetKeyDown(KeyCode.H))
+        {
+            ShowUIHelp();
         }
+    }
+
+    void ShowUIHelp()
+    {
+        helpUI.SetActive(!helpUI.activeInHierarchy);
+        Time.timeScale = helpUI.activeInHierarchy ? 0 : 1;
     }
 
     public UIScript GetUI() => gameUI.GetComponent<UIScript>();
